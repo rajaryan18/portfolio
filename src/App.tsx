3bff087cdb2d25a96e87f52e0b1a24a841cfb11b
Raj Aryan <73771884+rajaryan18@users.noreply.github.com>
2022-10-26 22:13:06 +0530

    image

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
     return (
         <>
             <Navbar />
-            <img src="https://github.com/rajaryan18/portfolio/blob/main/src/images/ring.png?raw=true" alt="Image" />
+            <img src="https://github.com/rajaryan18/portfolio/blob/main/src/images/rings.png?raw=true" alt="Image" className='ring-img' />
             {/* <Card>
                 <h1>HII</h1>
             </Card> */}
